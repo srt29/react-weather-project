1b37a0189a1d1dab26ac3f0995bbaea54dc94733
srt29 <143508194+srt29@users.noreply.github.com>
2024-03-11 01:55:43 +0200

    Added in structure of forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css"
 
@@ -10,6 +11,7 @@ export default function Weather(props) {
     function handleResponse(response) {
         setWeatherData({
             ready: true,
+            coordinates: response.data.coordinates,
             temperature: response.data.main.temp,
             wind: response.data.wind.speed,
             city: response.data.name,
@@ -50,6 +52,7 @@ export default function Weather(props) {
             </div>
             </form>
             <WeatherInfo data={weatherData}/>
+            <WeatherForecast coordinates={weatherData.coordinates}/>
     
         </div>
         );
