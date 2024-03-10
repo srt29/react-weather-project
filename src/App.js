e4e4d6969acbee839962cec1a760ff07e10af5c8
srt29 <143508194+srt29@users.noreply.github.com>
2024-03-10 14:09:57 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
      <h1>Weather App</h1>
      <footer>
       This project was coded by <a href='www.github.com/srt29' target='blank'>Sian Thumbadoo</a> and is <a href='https://github.com/srt29/react-weather-project' target='blank'>open-sourced on Github</a>
      </footer>
     </div>
+    </div>
   );
 }
 
 
+ 
