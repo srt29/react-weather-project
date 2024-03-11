ec99f40e061654d4291633ff1d0e6da67481074d
srt29 <143508194+srt29@users.noreply.github.com>
2024-03-11 03:19:04 +0200

    Completed 5 day forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -24,7 +24,7 @@ export default function WeatherForecastDay(props) {
     return (
       <div>
         <div className="WeatherForecast-day">{day()}</div>
-        <WeatherIcon code={props.data.weather[0].icon} size={36} />
+        <WeatherIcon code={props.data.weather[0].icon} size={100} />
         <div className="WeatherForecast-temperatures">
           <span className="WeatherForecast-temperature-max">
             {maxTemperature()}
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     icon={codeMapping[props.code]}
     color= "#1e1e1e"
     size= {props.size}
-animate={true} 
+    animate={true}
 />
 );
 }
