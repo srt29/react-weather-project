1b8ff3c15bdaf8be07686487d80cf995818c1fa9
srt29 <143508194+srt29@users.noreply.github.com>
2024-03-11 03:46:21 +0200

    Final changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <div className='container'>
      <Weather defaultCity="Durban"/>
      <footer className='footer'>
-      This project was coded by <a href='https://www.github.com/srt29' target='blank'>Sian Thumbadoo</a> and is <a href='https://github.com/srt29/react-weather-project' target='blank'>open-sourced on Github</a>
+      This project was coded by <a href='https://www.github.com/srt29' target='_blank' rel='noopener noreferrer'>Sian Thumbadoo</a> and is <a href='https://github.com/srt29/react-weather-project' target='_blank' rel='noopener no referrer'>open-sourced on Github</a>
      </footer>
     </div>
     </div>
